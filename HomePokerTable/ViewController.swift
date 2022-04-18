6f68cb2f69eeee63282aa1654dd88382891b466d
Sanghun <po4865@gmail.com>
2022-04-18 19:42:27 +0200

    init: Initiate new app project, Home-Poker-Table
    An iOS application that is a simple chip calculator when playing cards, like poker. On purpose of usage between friends and family those who play card games and want to have fun.

---
+++ HomePokerTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomePokerTable
+//
+//  Created by Sanghun Park on 18.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
